383fcad15e85f8f0cb929b3e32641dbdb5569219
RomainKH <romain.khanoyan@gmail.com>
2019-06-28 10:43:01 +0200

    :sparkles: articles done

--- front/article_2.php
+++ front/article_2.php
@@ -3,27 +3,37 @@
     <main class="page-wrapper">
         <article>
             <!-- Titre article dynamique -->
-            <h2 class="page-title">Intervation au Mali</h2>
+            <h2 class="page-title">Le conflit Syrien</h2>
             <!-- Date article dynamique -->
             <small class="article-date">mis à jour le 02/06/2019</small>
             
             <div class="article-main-image-mask">
                 <!-- Visuel Dynamique -->
-                <img class="article-main-image" src="./images/chine.jpg" alt="">
+                <img class="article-main-image" src="./images/syrie.jpg" alt="crimée">
             </div>
             <div class="article-content">
                 <!-- Sous titre Dynamique -->
-            <h3 class="article-subtitle">Sous titre</h3>
+            <h3 class="article-subtitle">La révolution syrienne</h3>
             <!-- Import de la data vis Dynamique -->
             <div class="article-data-visualisation">
-
+                <img src="./images/dataviz-syrie-1.png" alt="syrie data">
             </div>
-            <!-- Texte dynamique -->
-            <p class="paragraph">Lorem ipsum dolor amet tacos flexitarian flannel yuccie taxidermy tattooed schlitz scenester. Enamel pin scenester fam, fanny pack wayfarers stumptown direct trade locavore 3 wolf moon vinyl tbh bespoke meditation everyday carry kickstarter. Semiotics photo booth single-origin coffee, sustainable swag edison bulb bushwick kale chips dreamcatcher truffaut. Lorem ipsum dolor amet tacos flexitarian flannel yuccie taxidermy tattooed schlitz scenester. Enamel pin scenester fam, fanny pack wayfarers stumptown direct trade locavore 3 wolf moon vinyl tbh bespoke meditation everyday carry kickstarter.</p>
-            <br>
-            <p class="paragraph">Lorem ipsum dolor amet tacos flexitarian flannel yuccie taxidermy tattooed schlitz scenester. Enamel pin scenester fam, fanny pack wayfarers stumptown direct trade locavore 3 wolf moon vinyl tbh bespoke meditation everyday carry kickstarter. Semiotics photo booth single-origin coffee, sustainable swag edison bulb bushwick kale chips dreamcatcher truffaut. Lorem ipsum dolor amet tacos flexitarian flannel yuccie taxidermy tattooed schlitz scenester. Enamel pin scenester fam, fanny pack wayfarers stumptown direct trade locavore 3 wolf moon vinyl tbh bespoke meditation everyday carry kickstarter. Semiotics photo booth single-origin coffee, sustainable swag edison bulb bushwick kale chips dreamcatcher truffaut.</p>
-            <br>
-            <p class="paragraph">Lorem ipsum dolor amet tacos flexitarian flannel yuccie taxidermy tattooed schlitz scenester. Enamel pin scenester fam, fanny pack wayfarers stumptown direct trade locavore 3 wolf moon vinyl tbh bespoke meditation everyday carry kickstarter. Semiotics photo booth single-origin coffee, sustainable swag edison bulb bushwick kale chips dreamcatcher truffaut. Lorem ipsum dolor amet tacos flexitarian flannel yuccie taxidermy tattooed schlitz scenester. Enamel pin scenester fam, fanny pack wayfarers stumptown direct trade locavore 3 wolf moon vinyl tbh bespoke meditation everyday carry kickstarter.</p>
+                <!-- Texte dynamique -->
+                <p class="paragraph">Le pays compte 23 millions d’habitants et une superficie de 185 000 km². Il a été établi dans ses frontières actuelles après la Première guerre mondiale, suite au dépeçage de l’Empire ottoman par les Français et les Britanniques, au lendemain des accords de San Remo en 1920. Sous mandat français jusqu’en 1946, la Syrie indépendante a connu des épisodes démocratiques puis des coups d’Etats militaires dont celui du parti Baath en 1963 qui a établi le régime actuel. S’en suit alors un régime politique familial repris pas Bachar-El-Assad en 2000 suite à la mort de son père. Le peuple Syrien était donc sous un régime tyrannique depuis plus de 40 ans.</p>
+                <!-- subtitle -->
+                <h4>Les principales forces sur le terrain</h4>
+                <p class="paragraph">Au début de l’année 2011, le peuple Syrien fait part de son opinion face au régime actuel, cela fait suite au mouvement contestataire présent dans de nombreux pays à cette période (la Tunisie, l’Egypte, la Libye, le Bahreïn et le Yémen), ce sont les révolutions du “Printemps arabe”. Le point de départ du conflits entre le régime et le peuple Syrien est la torture d’enfants en raison de l’écrit d’un slogan qui disait “Le peuple veut la chute du régime”. Suite à cet évènement, les manifestations pour la démocratie et la fin de la corruption se propage dans tout le pays mais chaque jour qui passe voit la répression du régime devenir de plus en plus violente. En effet, 5000 victimes sont recensé par l’ONU dès 2011. Suite à la répression violente du régime de Bachar-El-Assad envers ses opposants, la rébellion syrienne décide de prendre les armes afin de se défendre de l’armée du régime ainsi que des bombardements.</p>
+                <p class="paragraph">Les forces gouvernementales, loyalistes au régime de Bachar Al-Assad comprennent aujourd’hui, outre l’armée régulière le régime est soutenue par 150.000 à 200.000 miliciens prorégime, y compris des Syriens, des Irakiens, des Iraniens et des Afghans. Leurs hélicoptères larguent quotidiennement des barils explosifs sur les zones rebelles, faisant des dizaines de victimes civiles. L’armée du régime dit se battre également contre Daech, mais en réalité très tardivement, bien moins fréquemment et avec moins de détermination et d’efficacité que contre les insurgés syriens.</p>*
+
+                <p class="paragraph">La Russie, alliée de poids de Damas, intervient depuis septembre 2015. Son implication a permis aux troupes gouvernementales de reprendre plusieurs zones clés, comme la ville d Alep et l enclave rebelle dans la Ghouta orientale, près de Damas.</p>
+                <p class="paragraph">L’Iran Autre allié de poids,a envoyé des milliers de combattants en Syrie et fournit des conseillers militaires.</p>
+                <p class="paragraph">L’opposition armée syrienne, appelée au début « Armée syrienne libre » (ASL), dite aussi « rébellion » ou « insurgés », n’a jamais réussi à se constituer en une force coordonnée et centralisée. Elle est composée de différents groupes et brigades locales, formés au départ par des civils qui ont pris les armes. Fortement dépendante de l’aide extérieure pour son financement et son équipement, elle a été dominée petit à petit par les islamistes plus ou moins radicaux. L’opposition armée n’a cessé de reculer depuis l’intervention russe à l’automne 2015. Après la perte de la moitié est d’Alep fin 2016, elle est essentiellement présente dans la région d’Idlib au nord-ouest de la Syrie le long de la frontière avec la Turquie.</p>
+                <p class="paragraph">L’Etat islamique ou Daech (selon son acronyme arabe), apparu sur le terrain à partir du printemps 2013, la redoutable formation jihadiste est composée à 80% de non Syriens, des extrémistes venus du monde entier.Elle contrôle toute la moitié orientale de la Syrie le long de la frontière avec l’Irak, berceau et base du mouvement. Elle s’est imposée par la force et la terreur, chassant les formations rebelles syriennes, notamment à Raqqa, sa capitale syrienne, soumettant la population locale à sa loi et commettant des atrocités. Attaquée depuis l’été 2014, par les raids aériens d’une coalition internationale menée par les Etats-Unis, puis depuis l’automne 2015 par les aviations russe, française, britannique, la formation jihadiste se trouve sur la défensive. Elle s’est projetée dès lors à travers le monde revendiquant des actions terroristes sanglantes et spectaculaires, notamment en France, mais aussi en Tunisie, en Egypte, etc…</p>
+                <p class="paragraph">Les forces kurdes syriennes, contrôlent la zone nord-ouest frontalière de la Turquie majoritairement peuplée de Kurdes syriens. Leurs forces militaires sont composées de combattants locaux. Ces forces kurdes luttent pour reconquérir leur territoire et pour l’autonomie, et ne sont pas considérées comme opposantes au régime syrien. Avec l’évolution du conflit, les Kurdes se sont retrouvés en première ligne pour lutter contre l’EI au Nord de la Syrie. Ils ont mené la grande bataille pour défendre et reconquérir la ville de Kobané. Une action rendue possible surtout grâce aux raids de l’aviation de la coalition internationale luttant contre Daech et aux livraisons d’armes, au grand dam d’Ankara. En juin 2015, les forces kurdes et des brigades de l’ASL ont réussi à évincer les hommes de l’Etat islamique de la ville et de la région de Tall Abyad, frontalière de la Turquie.</p>
+                <p class="paragraph">La coalition internationale emmenée par Washington, la coalition regroupe plus de 60 pays, dont la France et la Grande-Bretagne, et a mené des frappes aériennes contre l’EI depuis 2014, en appui à des troupes au sol. Avec la fin des grandes batailles contre les jihadistes, les frappes ont baissé en intensité. 2000 soldats américains ont été déployés dans le nord syrien, essentiellement des forces spéciales présentes pour combattre l’EI et entraîner les forces locales dans les zones reprises aux jihadistes.</p>
+            </div>
+            <div class="article-data-visualisation">
+                <img src="./images/dataviz-syrie-2.png" alt="syrie data">
             </div>
         </article>
         <button>
--- front/news.php
+++ front/news.php
@@ -16,16 +16,16 @@
             <span class="article__date">23/06/2019</span>
             <div class="resume">
                 <!-- TEXTE DYNAMIQUE -->
-                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
-                <a href="article.php" class="read__button">lire</a>
+                <p>Après les événements qui ont abouti à la destitution (22 février 2014) et à la fuite du président ukrainien (Viktor Ianoukovitch), on a vu émerger un mouvement d’autodéfense en Crimée, où une partie de la population s’est opposée au renversement du régime. Le 17 mars, à l’issue d’un référendum dont la légalité est largement mise en doute, le parlement criméen a proclamé l’indépendance de la Crimée et a demandé son rattachement à la fédération de Russie. Depuis, malgré la signature d’un accord à Genève...</p>
+                <a href="article_0.php" class="read__button">lire</a>
             </div>
             <div class="img__block color__indicator__blue">
                 <div class="img__frame">
                     <!-- IMAGE DYNAMIQUE -->
-                    <img src="images/chine.jpg" alt="chine">
+                    <img src="./images/crimee.jpg" alt="crimee">
                 </div>
                 <!-- TITRE DYNAMIQUE -->
-                <h3>Title</h3>
+                <h3>L’annexion de la Crimée</h3>
                 <div class="color__indicator"></div>
             </div>
         </div>
@@ -33,49 +33,33 @@
             <span class="article__date">23/06/2019</span>
             <div class="resume">
                 <!-- TEXTE DYNAMIQUE -->
-                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+                <p>Depuis le mois d’août 2017, des centaines de milliers de membres de la communauté ethnique musulmane Rohingya ont fui l’État de Rakhine (Arakan), une région située dans l’ouest de la Birmanie, afin d’échapper à des abus massifs commis par les forces de sécurité du gouvernement. Vers la mi-septembre 2017, environ 400 000 Rohingyas – soit près d’un tiers de la population totale Rohingya de Birmanie, estimée à 1,2 million de personnes – avaient franchi la frontière pour fuir vers le Bangladesh. Des dizaines de milliers d’autres Rohingyas se retrouvent déplacés à l’intérieur de la Birmanie, sans accès à une aide humanitaire vitale. En réponse à une attaque coordonnée menée par un groupe armé rohingya, l’armée birmane a lancé une campagne meurtrière recourant à des bombardements et à des incendies criminels, et qui s’apparente à une forme de nettoyage ethnique.</p>
                 <a href="article.php" class="read__button">lire</a>
             </div>
-            <div class="img__block color__indicator__red">
+            <div class="img__block color__indicator__blue">
                 <div class="img__frame">
                     <!-- IMAGE DYNAMIQUE -->
-                    <img src="images/chine.jpg" alt="chine">
+                    <img src="images/birmanie.jpg" alt="chine">
                 </div>
                 <!-- TITRE DYNAMIQUE -->
-                <h3>France</h3>
+                <h3>La crise des Rohingyas</h3>
             </div>
         </div>
         <div class="article__bulk__right">
-            <span class="article__date">23/06/2019</span>
+            <span class="article__date">05/06/2019</span>
             <div class="resume">
                 <!-- TEXTE DYNAMIQUE -->
-                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+                <p>Le pays compte 23 millions d’habitants et une superficie de 185 000 km². Il a été établi dans ses frontières actuelles après la Première guerre mondiale, suite au dépeçage de l’Empire ottoman par les Français et les Britanniques, au lendemain des accords de San Remo en 1920. Sous mandat français jusqu’en 1946, la Syrie indépendante a connu des épisodes démocratiques puis des coups d’Etats militaires dont celui du parti Baath en 1963 qui a établi le régime actuel. S’en suit alors un régime politique familial repris pas Bachar-El-Assad en 2000 suite à la mort de son père. Le peuple Syrien était donc sous un régime tyrannique depuis plus de 40 ans.</p>
                 <a href="article.php" class="read__button">lire</a>
             </div>
             <div class="img__block color__indicator__blue">
                 <div class="img__frame">
                     <!-- IMAGE DYNAMIQUE -->
-                    <img src="images/chine.jpg" alt="chine">
+                    <img src="images/syrie.jpg" alt="chine">
                 </div>
                 <!-- TITRE DYNAMIQUE -->
-                <h3>Title</h3>
+                <h3>Le conflit Syrien</h3>
                 <div class="color__indicator"></div>
-            </div>
-        </div>
-        <div class="article__bulk__left">
-            <span class="article__date">23/06/2019</span>
-            <div class="resume">
-                <!-- TEXTE DYNAMIQUE -->
-                <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
-                <a href="article.php" class="read__button">lire</a>
-            </div>
-            <div class="img__block color__indicator__red">
-                <div class="img__frame">
-                    <!-- IMAGE DYNAMIQUE -->
-                    <img src="images/chine.jpg" alt="chine">
-                </div>
-                <!-- TITRE DYNAMIQUE -->
-                <h3>France</h3>
             </div>
         </div>
     </div>
